7072d810dff2bab2829e0aa11caf76231855e9a6
gusadi09 <gusadi872@gmail.com>
2021-03-08 00:41:23 +0800

    finishing Register View and fix indent

--- TanamAja/ViewController/Authentication/Login/LoginViewController.swift
+++ TanamAja/ViewController/Authentication/Login/LoginViewController.swift
@@ -10,32 +10,69 @@ import UIKit
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: DesignableTextField!
     @IBOutlet weak var passTextField: DesignableTextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    private let button = UIButton(type: .custom)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-        emailTextField.layer.masksToBounds = true
-                emailTextField.layer.cornerRadius = 4
-                emailTextField.layer.borderWidth = 1
-        emailTextField.layer.borderColor = UIColor.systemGray3.cgColor
-                
-                passTextField.layer.masksToBounds = true
-                passTextField.layer.cornerRadius = 4
-                passTextField.layer.borderWidth = 1
-        passTextField.layer.borderColor = UIColor.systemGray3.cgColor
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.isHidden = true
     }
-    */
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    func setupUI() {
+        //MARK: TextField Setup
+        emailTextField.layer.masksToBounds = true
+        emailTextField.layer.cornerRadius = 4
+        emailTextField.layer.borderWidth = 1
+        emailTextField.layer.borderColor = UIColor.systemGray3.cgColor
+        
+        passTextField.layer.masksToBounds = true
+        passTextField.layer.cornerRadius = 4
+        passTextField.layer.borderWidth = 1
+        passTextField.layer.borderColor = UIColor.systemGray3.cgColor
+        
+        button.setImage(UIImage(named: "look"), for: .normal)
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 15)
+        button.frame = CGRect(x: CGFloat(passTextField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
+        button.addTarget(self, action: #selector(self.openPass), for: .touchUpInside)
+        passTextField.rightView = button
+        passTextField.rightViewMode = .always
+        
+        //MARK: LoginBtn Setup
+        loginBtn.clipsToBounds = true
+        loginBtn.layer.cornerRadius = 5
+        loginBtn.layer.shadowColor = UIColor(named: "Green")?.cgColor
+        loginBtn.layer.shadowOffset = CGSize(width: 0.0, height: 4.0)
+        loginBtn.layer.shadowOpacity = 0.25
+        loginBtn.layer.shadowRadius = 12
+        loginBtn.layer.masksToBounds = false
+        
+        //MARK: NavigationBar Setup
+        navigationItem.backButtonTitle = ""
+    }
+    
+    @IBAction func openPass(_ sender: Any) {
+        if passTextField.isSecureTextEntry {
+            passTextField.isSecureTextEntry = false
+            button.setImage(UIImage(named: "lookclick"), for: .normal)
+        } else {
+            passTextField.isSecureTextEntry = true
+            button.setImage(UIImage(named: "look"), for: .normal)
+        }
+    }
+    
+    @IBAction func moveToRegister(_ sender: UIButton) {
+        performSegue(withIdentifier: "toRegister", sender: self)
+    }
 }

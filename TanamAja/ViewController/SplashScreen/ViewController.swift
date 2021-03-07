61d4e4e0aae19bed5558237837f7a8a325f53efc
gusadi09 <gusadi872@gmail.com>
2021-03-07 18:13:11 +0800

    input asset and make getting started view

--- TanamAja/ViewController/SplashScreen/ViewController.swift
+++ TanamAja/ViewController/SplashScreen/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    func setupUI() {
+        startButton.clipsToBounds = true
+        startButton.layer.cornerRadius = 10
+        startButton.layer.masksToBounds = false
+        
+        navigationController?.navigationBar.isHidden = true
     }
 
-
+    @IBAction func startPressed(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "MainView", bundle: nil)
+        
+        let vc = storyboard.instantiateViewController(identifier: "LoginVC") as? LoginViewController
+        
+        self.navigationController?.setViewControllers([vc!], animated: true)
+    }
 }
 
